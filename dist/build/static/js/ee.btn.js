118fc363d883bd5239d4f8c8fa5fc79fcb516983
leonvv <l.volokitin@macte.pro>
2025-06-05 12:28:15 +0300

    Update ee.btn.js

--- dist/build/static/js/ee.btn.js
+++ dist/build/static/js/ee.btn.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 	document.body.innerHTML += '<div id="entity_editor" style="position:absolute;padding:0;margin:0;top:0;left:0;width:100%;height:100%;background:rgba(255,255,255,1);z-index:10;display:none;"></div>';
-	document.body.innerHTML += '<button onClick="document.getElementById(\'entity_editor\').style.display=\'block\'" type="button" class="ant-btn ant-btn-danger" style="position: fixed; top: 40px; right: 20px;">Open Entity Editor</button>';
+	document.body.innerHTML += '<button onClick="document.getElementById(\'entity_editor\').style.display=\'block\'" type="button" class="ant-btn ant-btn-danger" style="position: fixed; top: 240px; right: 50px;">Open Entity Editor</button>';
 	var head = document.getElementsByTagName('head')[0];
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
